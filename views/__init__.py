d0f8a5577845d913df3073e7103b99e023bf618a
Nick Valdez <nicholastvaldez@gmail.com>
2023-01-12 14:34:18 -0600

    capter one complete

--- views/__init__.py
+++ views/__init__.py
@@ -1,4 +1,4 @@
-from .metals import get_all_metals, get_single_metal
-from .sizes import get_all_sizes, get_single_size
-from .styles import get_all_styles, get_single_style
-from .orders import get_all_orders, get_single_order, create_order, delete_order, update_order
+from .metals_requests import get_all_metals, get_single_metal
+from .sizes_requests import get_all_sizes, get_single_size
+from .styles_requests import get_all_styles, get_single_style
+from .orders_requests import get_all_orders, get_single_order, create_order, delete_order, update_order
